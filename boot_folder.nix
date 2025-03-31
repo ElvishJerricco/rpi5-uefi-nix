ebfce127de16476cdf2fd7706c39afed37ce0057
Will Fancher <elvishjerricco@gmail.com>
2025-03-31 19:52:55 -0400

    nixfmt

--- boot_folder.nix
+++ boot_folder.nix
@@ -1,8 +1,8 @@
 { rpi5-uefi, runCommand }:
 
-runCommand "boot_folder" {} ''
-mkdir $out
-cd $out
-cp ${./config.txt} config.txt
-cp ${rpi5-uefi.fd}/FV/RPI_EFI.fd RPI_EFI.fd
+runCommand "boot_folder" { } ''
+  mkdir $out
+  cd $out
+  cp ${./config.txt} config.txt
+  cp ${rpi5-uefi.fd}/FV/RPI_EFI.fd RPI_EFI.fd
 ''
